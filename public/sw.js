bac4e0a6ed2e67a96f61f965a2619e586cc4bd02
optixzdude-maker <optixzdude@gmail.com>
2026-02-18 22:02:48 -0600

    Delete public/sw.js

--- public/sw.js
+++
@@ -1,8 +0,0 @@
-self.addEventListener('install', (e) => {
-  console.log('Service Worker Installed');
-});
-
-self.addEventListener('fetch', (e) => {
-  // Keeps the app responsive
-  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
-});
